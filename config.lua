6f72a8d4429fd722705f93b0accb591b500ba128
BayloraiEX <117157428+BayloraiEX@users.noreply.github.com>
2026-09-09 16:19:16 -0700

    Update to 1.5

--- config.lua
+++ config.lua
@@ -7,7 +7,6 @@ config.oxtarget = false
 -------------------------------------------------------
 
 config.InventorySystem = 'qb' -- Supports 'ox' & 'qb'
-config.useBlip = true
 config.pedmodel = 'a_m_m_prolhost_01' -- ped model hash
 
 config.scenario = 'WORLD_HUMAN_CLIPBOARD' -- scenario for ped to play, false to disable
@@ -23,14 +22,20 @@ config.locations = {
         maxZ = 30.81,  
         debug = false, 
         -----------------------------------------------------
+        -------- blip (each location can have its own) --------
+        useBlip = true,   -- set to false to hide the blip for this location only
+        blipSprite = 326,
+        blipScale = 0.4,
+        blipColor = 48,   -- see https://docs.fivem.net/docs/game-references/blips/#blip-colors
+        ---------------------------------------------------------
         vehicles = {
             ['panto'] = {
                 price = 50,
-                image = 'https://i.imgur.com/vuP5xMc.jpeg',
+                image = 'https://docs-backend.fivem.net/vehicles/panto.webp',
             },
             ['bmx'] = {
                 price = 0,
-                image = 'https://i.imgur.com/TKTtwYF.jpeg',
+                image = 'https://docs-backend.fivem.net/vehicles/bmx.webp',
             },
         },
         vehiclespawncoords = vector4(212.64, -797.12, 30.87, 339.09), -- where vehicle spawns when rented
@@ -45,14 +50,20 @@ config.locations = {
         maxZ = 30.81,  
         debug = false, 
         -----------------------------------------------------
+        -------- blip (each location can have its own) --------
+        useBlip = true,
+        blipSprite = 326,
+        blipScale = 0.4,
+        blipColor = 48,
+        ---------------------------------------------------------
         vehicles = {
             ['panto'] = {
                 price = 50,
-                image = 'https://i.imgur.com/vuP5xMc.jpeg',
+                image = 'https://docs-backend.fivem.net/vehicles/panto.webp',
             },
             ['bmx'] = {
                 price = 0,
-                image = 'https://i.imgur.com/TKTtwYF.jpeg',
+                image = 'https://docs-backend.fivem.net/vehicles/bmx.webp',
             },
         },
         vehiclespawncoords = vector4(253.77, -564.2, 43.27, 152.49),
@@ -60,3 +71,12 @@ config.locations = {
     -- add as many locations as you'd like with any type of vehicle (air, water, land) follow same format as above
 }
 
+config.rentalDurations = {
+    {label = '30 Minutes', minutes = 30},
+    {label = '1 Hour',     minutes = 60},
+    {label = '2 Hours',    minutes = 120},
+    {label = '4 Hours',    minutes = 240},
+}
+
+config.returnRefundPercent = 0.75 -- paid back when the player manually uses "Return Vehicle"
+config.expireRefundPercent = 0.25 -- paid back when the rental timer runs out on its own
